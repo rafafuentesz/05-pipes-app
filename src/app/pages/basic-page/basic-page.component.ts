0bc6deb3a1893015b8ccf0cd81bf8351fd9aaa11
rafafuentesz <rafafuentes110@gmail.com>
2025-08-28 11:35:53 -0300

    basic page component and number page component updated

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicPageComponent { }
+export default class BasicPageComponent {
+  nameLower = signal('rafael');
+  nameUpper = signal('RAFAEL');
+  fullName = signal('rAFaeL fuENTes');
+
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date())
+    },1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    })
+  })
+}
