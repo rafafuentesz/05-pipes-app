6740be6bb9d487281989ff03726281a86efc2928
rafafuentesz <rafafuentes110@gmail.com>
2025-08-28 10:34:46 -0300

    feat: restructure app layout and add routing
    - Updated app.html to include a navbar and router outlet for navigation.
    - Defined new routes for basic, numbers, uncommon, and custom pages in app.routes.ts.
    - Created NavbarComponent to dynamically generate navigation links based on defined routes.
    - Implemented basic, numbers, uncommon, and custom page components with placeholder content.
    - Added Tailwind CSS and DaisyUI for styling in styles.css.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'basic',
+    title: 'Pipes Básicos',
+    loadComponent: () => import('./pages/basic-page/basic-page.component')
+  },
+  {
+    path: 'numbers',
+    title: 'Pipes numéricos',
+    loadComponent: () => import('./pages/numbers-page/numbers-page.component')
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes no tan comunes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page.component')
+  },
+  {
+    path: 'custom',
+    title: 'Pipes personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page.component')
+  },
+  {
+    path: '**',
+    redirectTo: 'basic'
+  }
+
+
+];
