80f876825f6667c5188edd5a9cdf89f0edc511f3
rafafuentesz <rafafuentes110@gmail.com>
2025-09-04 16:11:31 -0300

    Agregar componente ToggleCase y su respectivo pipe para alternar entre mayúsculas y minúsculas

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [
+    ToggleCasePipe
+
+  ],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+  name = signal('Rafael Fuentes');
+
+
+  upperCase = signal(true);
+
+  toggleCase() {
+    this.upperCase.set(!this.upperCase());
+  }
+ }
